4f88ebebd4e78d92ddfa709b6a9bb26887487966
Manuel Jesús Márquez Benítez <marqben@gmail.com>
2020-10-13 19:41:13 +0200

    hamming

--- Hamming.php
+++ Hamming.php
@@ -1,12 +1,26 @@
 <?php
+/**
+ * @author Manuel Jesús Márquez Benítez <manueljesus.marquez@iesdonana.org>
+ * @copyright Copyright (c) 2020 Manuel Jesús Márquez Benítez
+ * @license 
+ */
 
 /*
- * Esto es sólo un ESQUELETO para el ejercicio de la "distancia Hamming".
+ * Calcula la distancia Hamming entre dos cadenas de ADN.
+ * 
+ * @param string $a La primera cadena. 
+ * @param string $b La segunda cadena. 
+ * @return int|bool El número de caracteres distintos 
+ *                  https://github.com/Manumarqben/isograma-ricpelo.gito false si tienen distinta longitud. 
  */
 
 function distancia($a, $b)
 {
-    //
-    // TU CÓDIGO VA AQUÍ
-    //
+    if (mb_strlen($a) != mb_strlen($b))
+    {
+        return false;
+    }
+
+    return count(array_diff_assoc(mb_str_split($a), mb_str_split($b)));
+
 }
